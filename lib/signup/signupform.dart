65eef3263e06ad7afc022efb2bd2df5e8a293f16
Vinayak Gupta <98951422+vinayakgupta29@users.noreply.github.com>
2022-10-21 15:41:02 +0530

    hero animation from the main screen
    hero animation from the back screen and a back button is added

--- lib/signup/signupform.dart
+++ lib/signup/signupform.dart
@@ -3,40 +3,63 @@ import 'package:flutter/material.dart';
 import '/already_have_acc.dart';
 import '../../../constants.dart';
 import '../../login/loginscreen.dart';
+import 'signupscreen.dart';
+import 'package:signupdemo/email_field_widget.dart';
 
-class SignUpForm extends StatelessWidget {
-  const SignUpForm({
-    Key? key,
-  }) : super(key: key);
+
+class Signupformapp extends StatelessWidget{
+  static const String title ='Email Fiels';
+
+  const Signupformapp({Key? key}) : super(key: key);
+  @override 
+  Widget build(BuildContext context) => const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: title,
+    home:  SignUpForm(),
+  );
+}
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SignUpFormState createState()=> _SignUpFormState();
+}
+class _SignUpFormState extends State<SignUpForm>{
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Form(
+        key:formKey,
         child: Column(
           children: [
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              textInputAction: TextInputAction.next,
-              cursorColor: kPrimaryColor,
-              onSaved: (email) {},
-              decoration: const InputDecoration(
-                hintText: "Your email",
-                prefixIcon: Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                  child: Icon(Icons.person),
-                ),
-              ),
-            ),
+            Image.asset("assets/icons/1.png",
+            height: 100 ,),
+            const SizedBox(height: 100,),
+
+            EmailFieldWidget(controller: emailController),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: defaultPadding),
               child: TextFormField(
                 textInputAction: TextInputAction.done,
                 obscureText: true,
                 cursorColor: kPrimaryColor,
-                decoration: const InputDecoration(
+                decoration:  InputDecoration(
                   hintText: "Your password",
-                  prefixIcon: Padding(
+                  border:OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  prefixIcon: const Padding(
                     padding: EdgeInsets.all(defaultPadding),
                     child: Icon(Icons.lock),
                   ),
@@ -44,10 +67,16 @@ class SignUpForm extends StatelessWidget {
               ),
             ),
             const SizedBox(height: defaultPadding / 2),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Sign Up".toUpperCase()),
+            Hero(
+            tag:'singup_btn',
+            child: ElevatedButton(
+              onPressed: () { },
+              style: ElevatedButton.styleFrom(
+                  primary: kPrimaryColor, elevation: 0),
+              child: Text(
+                "Sign Up".toUpperCase()),
             ),
+          ),
             const SizedBox(height: defaultPadding),
             AlreadyHaveAnAccountCheck(
               login: false,
